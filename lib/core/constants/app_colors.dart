5ea8aa7cb7f353ada1766cb4b32b7b86fa2311a4
Mahabub Tamim <102644237+Tamimbd2@users.noreply.github.com>
2025-10-22 17:58:38 +0600

    Update app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-    static const background = Color(0xFFF9FAFB);
+  static const background = Color(0xFFF9FAFB);
   static const textDark = Color(0xFF111827);
   static const textLight = Color.fromARGB(255, 112, 118, 129);
   static const primaryLight = Color.fromARGB(255, 33, 150, 243);
@@ -9,6 +9,5 @@ class AppColors {
   static const primary = Color.fromARGB(255, 25, 99, 247);
   static const background = Color(0xFFF9FAFB);
   static const textDark = Color(0xFF111827);
-
   static Color? get primaryBlue => null;
 }
